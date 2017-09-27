cfe5ad93b6f8802ad29798bd0b5f35a12042f9a0
DeemoHarlos <cces4415@gmail.com>
2017-09-27 11:42:14 +0800

    mainpage wireframe

---
+++ index.js
@@ -0,0 +1,10 @@
+function login(){
+	document.getElementById('loginpage').style.display = 'none';
+	document.getElementById('mainpage').style.display = 'block';
+}
+function showpanel(){
+	document.getElementById('functionpanel').style.left = '0';
+}
+function hidepanel(){
+	document.getElementById('functionpanel').style.left = '-70%';
+}
